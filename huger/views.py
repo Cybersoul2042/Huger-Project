dc780e64edd6663510976cd29c05b50cd8de6e36
Cybersoul2042 <amirsekiroguru@gmail.com>
2024-02-02 20:45:26 +0330

    Commit

--- huger/views.py
+++ huger/views.py
@@ -12,7 +12,9 @@ from .models import User
 
 def index(request):
     if request.user.is_authenticated:
-        return render(request, 'huger/home.html')
+        return render(request, 'huger/home.html', {
+            'user': request.user
+        })
     else:
         return HttpResponseRedirect(reverse("login"))
     
