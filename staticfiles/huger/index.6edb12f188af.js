8e88c71082903179877dee771cc4035e4ad4eac7
Cybersoul2042 <amirsekiroguru@gmail.com>
2024-02-04 19:20:19 +0330

    Static Files To Web

---
+++ staticfiles/huger/index.6edb12f188af.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var _a;
+    (_a = document.getElementById('home-btn')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => ChangePage('home-btn'));
+});
+function ChangePage(pagetype) {
+    const desiredPage = document.getElementById('');
+}
+//# sourceMappingURL=index.js.2737fda4ea5a.map
